b59c709004cf874ab5de244fa33659b4df61c018
rohandeshmukh1 <rohandeshmukh1@gmail.com>
2017-05-06 12:30:28 -0400

    adding code with new dbuser class hence with some errors

--- src/NewsfeedServelet.java
+++ src/NewsfeedServelet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+import customTools.DbUser;
 /**
  * Servlet implementation class NewsfeedServelet
  */
--- src/ProfileServlet.java
+++ src/ProfileServlet.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
+import customTools.DbUser;
 @WebServlet("/ProfileServlet")
 public class ProfileServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
